5f642f9aa7865e82a270d04a1d1cfed7182a072d
sorapan <t.sorapan@gmail.com>
2013-11-30 17:54:38 +0700

    new print option's box

--- print_option_1.js
+++ print_option_1.js
@@ -3,12 +3,16 @@ $(function() {
     document.getElementById("last_number").disabled = true;
     document.getElementById("last_alphabet").disabled = true;
 
-    $(" #btg").click(function(){
+//    $(document).on("click"," #btg"
+
+    $(document).on("click"," #btg",function(){
 
         var new_height = ($(' #bottom_box ').height() == 90) ? $(" #btg").height() : 90;
+        var new_width = ($(' #bottom_box ').width() == $(window).width()) ? 250 : $(window).width();
 
         $(" #bottom_box").animate({
-           height : new_height
+            height : new_height,
+            width : new_width
         });
 
     });
